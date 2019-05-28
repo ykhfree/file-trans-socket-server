e44df11ee3e29b7b69d862e0b3bfaeca34bbdd69
kanghyun.yang <ykhfree@gmail.com>
2019-05-28 18:42:04 +0900

    feat : Netty를 이용한 파일 전송 client 등록

--- package/bin/check.sh
+++ package/bin/check.sh
@@ -14,7 +14,7 @@ then
 else
     RUN=../lib/file_trans_socket-0.8.jar
     CLASSPATH=$RUN
-	nohup java -Xms512m -Xmx512m -XX:+UseParallelGC -XX:+UseParallelOldGC -classpath $CLASSPATH ykhfree.dev.transfile.app.SocketServer 1> /dev/null 2>&1 &
+	nohup java -Xms512m -Xmx512m -XX:+UseParallelGC -XX:+UseParallelOldGC -classpath $CLASSPATH ykhfree.dev.transfile.server.app.SocketServer 1> /dev/null 2>&1 &
 	MyPID=$!
 	echo "Socket Server start!!"
 	echo "kill $MyPID" > shutdown.sh
--- package/bin/startup.sh
+++ package/bin/startup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 RUN=../lib/file_trans_socket-0.8.jar
 CLASSPATH=$RUN
-nohup java -Xms512m -Xmx512m -XX:+UseParallelGC -XX:+UseParallelOldGC -classpath $CLASSPATH ykhfree.dev.transfile.app.SocketServer 1> /dev/null 2>&1 &
-#java -Xms512m -Xmx512m -XX:+UseParallelGC -XX:+UseParallelOldGC -classpath $CLASSPATH ykhfree.dev.transfile.app.SocketServer
+nohup java -Xms512m -Xmx512m -XX:+UseParallelGC -XX:+UseParallelOldGC -classpath $CLASSPATH ykhfree.dev.transfile.server.app.SocketServer 1> /dev/null 2>&1 &
+#java -Xms512m -Xmx512m -XX:+UseParallelGC -XX:+UseParallelOldGC -classpath $CLASSPATH ykhfree.dev.transfile.server.app.SocketServer
 
 MyPID=$!
 echo "Socket Server start!!"
